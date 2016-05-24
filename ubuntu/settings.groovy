d265e3647488b09bbf0fed49e144334c53602cc0
Steffen Bleul <blacklabelops@itbleul.de>
2016-05-24 15:03:34 +0200

    Added ubuntu base image.

---
+++ ubuntu/settings.groovy
@@ -0,0 +1,21 @@
+/**
+ * Jenkins 2.0 Buildfile
+ **/
+
+/**
+ * Build parameters, must be adjusted when forked!
+ **/
+dockerImageName = 'blacklabelops/ubuntu'
+dockerWorkspace = 'ubuntu/'
+dockerTags = ["latest","16.04"] as String[]
+dockerTestCommands =
+ ["echo hello world",
+  "ps -All",
+  "uname -r",
+  "whoami",
+  "cat /etc/hosts",
+  "cat /etc/passwd"] as String[]
+dockerRepositories = [["","Dockerhub","DockerhubEmail"]] as String[][]
+dockerImages = ["ubuntu:16.04"] as String[]
+
+return this;
