58d77f1b2fc2f38fbd4142327691e0e68ee6056b
Steffen Bleul <sbl@itbleul.de>
2016-06-19 10:36:50 +0200

    New alpine version.

--- alpine/Dockerfile
+++ alpine/Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.3
+FROM alpine:3.4
 MAINTAINER Steffen Bleul <sbl@blacklabelops.com>
 
 RUN apk upgrade --update && \
--- alpine/settings.groovy
+++ alpine/settings.groovy
@@ -7,7 +7,7 @@
  **/
 dockerImageName = 'blacklabelops/alpine'
 dockerWorkspace = 'alpine/'
-dockerTags = ["latest","3.3"] as String[]
+dockerTags = ["latest","3.4"] as String[]
 dockerTestCommands =
  ["echo hello world",
   "ps -All",
@@ -16,6 +16,6 @@ dockerTestCommands =
   "cat /etc/hosts",
   "cat /etc/passwd"] as String[]
 dockerRepositories = [["","Dockerhub","DockerhubEmail"]] as String[][]
-dockerImages = ["alpine:3.3"] as String[]
+dockerImages = ["alpine:3.4"] as String[]
 
 return this;
