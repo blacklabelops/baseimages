90f2439953f2de0ba6812dd65db6b4d57e043c48
Steffen Bleul <sbl@itbleul.de>
2017-02-18 21:00:49 +0100

    Alpine 3.4 branch for go projects.

--- alpine/settings.groovy
+++ alpine/settings.groovy
@@ -7,7 +7,7 @@
  **/
 dockerImageName = 'blacklabelops/alpine'
 dockerWorkspace = 'alpine/'
-dockerTags = ["latest","3.5"] as String[]
+dockerTags = ["3.4"] as String[]
 dockerTestCommands =
  ["echo hello world",
   "ps -All",
@@ -18,6 +18,6 @@ dockerTestCommands =
   "/sbin/tini -h",
   "su-exec"] as String[]
 dockerRepositories = [["","Dockerhub","DockerhubEmail"]] as String[][]
-dockerImages = ["alpine:3.5"] as String[]
+dockerImages = ["alpine:3.4"] as String[]
 
 return this;
