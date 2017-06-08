4a4417cbc68ff37796588d670bed02ad14f1a6c7
Steffen Bleul <sbl@itbleul.de>
2017-06-08 22:19:12 +0200

    Fixing branch to tag 3.5

--- alpine/settings.groovy
+++ alpine/settings.groovy
@@ -7,7 +7,7 @@
  **/
 dockerImageName = 'blacklabelops/alpine'
 dockerWorkspace = 'alpine/'
-dockerTags = ["latest","3.5"] as String[]
+dockerTags = ["3.5"] as String[]
 dockerTestCommands =
  ["echo hello world",
   "ps -All",
