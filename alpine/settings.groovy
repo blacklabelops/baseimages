a34efa73985764cd7db0f50a3cdb9d6ba76247f6
Steffen Bleul <sbl@blacklabelops.com>
2017-12-07 19:16:16 +0100

    Branch should only build tagged version.

--- alpine/settings.groovy
+++ alpine/settings.groovy
@@ -7,7 +7,7 @@
  **/
 dockerImageName = 'blacklabelops/alpine'
 dockerWorkspace = 'alpine/'
-dockerTags = ["latest","3.7"] as String[]
+dockerTags = ["3.7"] as String[]
 dockerTestCommands =
  ["echo hello world",
   "ps -All",
